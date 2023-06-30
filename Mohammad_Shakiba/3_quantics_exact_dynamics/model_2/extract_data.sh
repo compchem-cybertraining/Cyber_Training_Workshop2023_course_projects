b644c4729dbc110ec1d96fd677108a86b9373167
MohammadShakiba <mshakiba.kerman.iran@gmail.com>
2023-06-30 16:05:51 -0400

    Removed output folder model_2

--- Mohammad_Shakiba/3_quantics_exact_dynamics/model_2/extract_data.sh
+++
@@ -1,5 +0,0 @@
-cat output | grep 'state = 1  pop' | awk '{print $5}' > pop_1.txt
-cat output | grep 'state = 2  pop' | awk '{print $5}' > pop_2.txt
-cat output | grep 'state = 2  pop' | awk '{print $11}' > ene_2.txt
-cat output | grep 'state = 1  pop' | awk '{print $11}' > ene_1.txt
-cat output | grep '<q>' | awk '{print $4}' > q.txt
