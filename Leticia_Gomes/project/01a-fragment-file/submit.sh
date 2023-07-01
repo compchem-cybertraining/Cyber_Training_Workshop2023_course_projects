244c69118bfd9a4668343f561aecc9c073eee2fd
adaogomesl <100699955+adaogomesl@users.noreply.github.com>
2023-06-30 22:35:42 -0400

    Add files via upload

---
+++ Leticia_Gomes/project/01a-fragment-file/submit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --account=cyberwksp21
+#SBATCH --partition=valhalla  --qos=valhalla
+#SBATCH --clusters=faculty
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=24
+#SBATCH -C CPU-E5-2650v4
+module purge
+eval "$(/projects/academic/cyberwksp21/Software/nwchem_conda0/bin/conda shell.bash hook)"
+mpirun -n ${SLURM_NTASKS} nwchem input.inp
